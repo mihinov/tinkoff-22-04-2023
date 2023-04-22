0ffbed137791672cd4033bafcac198f5a7361622
mihinov <kamahinmihail@gmail.com>
2023-04-22 17:32:31 +0300

    Вроде добавил анимацию

--- src/app/components/expense-list/expense-list.component.ts
+++ src/app/components/expense-list/expense-list.component.ts
@@ -14,7 +14,10 @@ import { animate, style, transition, trigger } from '@angular/animations';
       transition(':enter', [
         style({ opacity: 0 }),
         animate('500ms', style({ opacity: 1 }))
-			])
+			]),
+			transition(':leave',
+        animate('500ms', style({ opacity: 0 }))
+      )
     ])
 	]
 })
