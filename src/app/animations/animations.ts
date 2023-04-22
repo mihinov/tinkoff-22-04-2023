efd8de5f451e2aeae81387f05568de1eb9e9209f
mihinov <kamahinmihail@gmail.com>
2023-04-22 17:31:28 +0300

    пытаюсь добавить анимацию

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -8,42 +8,42 @@ export const routeAnimations = trigger('routeAnimations', [
 				left: 0,
 				width: '100%',
 				opacity: 0,
-				transform: 'translateY(100%)'
 			})
 		]),
 		query(':enter', [
 			animate('600ms ease',
-				style({ opacity: 1, transform: 'translateY(0)' })
+				style({ opacity: 1 })
 			)
 		])
 	])
 ]);
 
-export const slider = trigger('routeAnimations', [
-	transition('* => isLeft', slideTo('left')),
-	transition('* => isRight', slideTo('right')),
-	transition('isRight => *', slideTo('left')),
-	transition('isLeft => *', slideTo('right'))
-]);
+// export const slider = trigger('routeAnimations', [
+// 	transition('* => isLeft', slideTo('left')),
+// 	transition('* => isRight', slideTo('right')),
+// 	transition('isRight => *', slideTo('left')),
+// 	transition('isLeft => *', slideTo('right'))
+// ]);
 
-function slideTo(direction: 'left' | 'right') {
-	const optional = { optional: true };
-	return [
-		query(':enter, :leave', [
-			style({
-				position: 'absolute',
-				top: 0,
-				[direction]: 0,
-				width: '100%'
-			})
-		], optional),
-		query(':enter', [
-			style({ [direction]: '-100%' } )
-		]),
-		group([
-			query(':leave', [
-				animate('600ms ease', style({ [direction]: '100%' }))
-			])
-		])
-	];
-}
+// function slideTo(direction: 'left' | 'right') {
+// 	const optional = { optional: true };
+// 	return [
+// 		query(':enter, :leave', [
+// 			style({
+// 				// position: 'absolute',
+// 				// top: 0,
+// 				// [direction]: 0,
+// 				// width: '100%'
+// 				opacity: 0
+// 			})
+// 		], optional),
+// 		query(':enter', [
+// 			style({ opacity: 0 } )
+// 		]),
+// 		group([
+// 			query(':leave', [
+// 				animate('600ms ease', style({ [direction]: '100%' }))
+// 			])
+// 		])
+// 	];
+// }
